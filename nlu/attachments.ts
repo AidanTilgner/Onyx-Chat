5783bbd66937b02dabf9415d0902d52972605ccc
Aidan Tilgner <aidantilgner02@gmail.com>
2022-11-27 23:16:04 -0800

    added buttons

--- nlu/attachments.ts
+++ nlu/attachments.ts
@@ -1,7 +1,9 @@
 export const extractAttachments = async (text: string) => {
-  const linkData = await extractLinks(text);
+  const linkData = extractLinks(text);
+  const buttons = extractButtons(text);
   return {
     links: linkData,
+    buttons,
   };
 };
 
@@ -15,4 +17,22 @@ export const extractLinks = (text: string) => {
     return { href, text, domain };
   });
   return linkData;
+};
+
+export const extractButtons = (text: string) => {
+  // match << __button.<button_type> >> in text
+  const buttons = text.match(/<<\s*__button\.(.*?)\s*>>/g);
+  const buttonData = buttons?.map((button) => {
+    // might have more data in __metadata{key: value} format
+    const metadata = button.match(/__metadata\{(.*?)\}/)?.[1];
+    const parsed = metadata?.split(",").reduce((acc, curr) => {
+      const [key, value] = curr.split(":");
+      return { ...acc, [key]: value };
+    }, {});
+    // type should only be the text immediately after __button. and before __metadata
+    const type = button.match(/__button\.(.*?) __metadata/)?.[1];
+    return { type, metadata: parsed };
+  });
+
+  return buttonData;
 };
